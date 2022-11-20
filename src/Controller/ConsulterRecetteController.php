91db39d4104f74751b7ffc8910205bcba68fdf73
Thomas Thirion <thirionthomas68100@gmail.com>
2022-11-20 14:13:57 +0100

    card recette ok

---
+++ src/Controller/ConsulterRecetteController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recette;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConsulterRecetteController extends AbstractController
+{
+    #[Route('/consulter/recette/{id}', name: 'consulter_recette')]
+    public function index(ManagerRegistry $managerRegistry, $id): Response
+    {
+        $recette= $managerRegistry->getRepository(Recette::class)->findOneBy(array('id' => $id));
+
+        return $this->render('consulter_recette/index.html.twig',[
+            'recette'=>$recette
+        ]);
+    }
+}
